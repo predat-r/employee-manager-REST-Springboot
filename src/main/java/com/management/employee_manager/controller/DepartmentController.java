39dd0eb42932796cbfb85905eabba77637d78d01
haris.naeem <harisnaeem612@outlook.com>
2026-07-09 11:07:08 +0500

    add update department endpoint

--- src/main/java/com/management/employee_manager/controller/DepartmentController.java
+++ src/main/java/com/management/employee_manager/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package com.management.employee_manager.controller;
 import com.management.employee_manager.dto.DepartmentRequestDto;
 import com.management.employee_manager.dto.DepartmentResponseDto;
 import com.management.employee_manager.service.DepartmentService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,5 +31,18 @@ public class DepartmentController {
     public ResponseEntity<List<DepartmentResponseDto>> getAllDepartments(){
         List<DepartmentResponseDto> departmentResponseDtos = departmentService.getAllDepartments();
         return ResponseEntity.ok().body(departmentResponseDtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DepartmentResponseDto> getDepartmentById(@PathVariable Long id) {
+        DepartmentResponseDto departmentResponseDto =
+                departmentService.getDepartmentById(id);
+
+        return ResponseEntity.ok(departmentResponseDto);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<DepartmentResponseDto> updateDepartment(@PathVariable Long id,@RequestBody DepartmentRequestDto departmentRequestDto){
+        DepartmentResponseDto departmentResponseDto = departmentService.updateDepartment(id,departmentRequestDto);
+        return ResponseEntity.ok().body(departmentResponseDto);
     }
 }
--- src/main/java/com/management/employee_manager/service/DepartmentService.java
+++ src/main/java/com/management/employee_manager/service/DepartmentService.java
@@ -8,17 +8,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DepartmentService {
-private final DepartmentRepository departmentRepository;
+    private final DepartmentRepository departmentRepository;
 
     public DepartmentService(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
     }
 
-    public DepartmentResponseDto createDepartment(DepartmentRequestDto requestDto){
-        Department department = new Department(requestDto.getName(),requestDto.getDescription());
+    public DepartmentResponseDto createDepartment(DepartmentRequestDto requestDto) {
+        Department department = new Department(requestDto.getName(), requestDto.getDescription());
         Department savedDepartment = departmentRepository.save(department);
 
         return new DepartmentResponseDto(
@@ -27,16 +28,38 @@ private final DepartmentRepository departmentRepository;
                 savedDepartment.getDescription()
         );
     }
-    public List<DepartmentResponseDto> getAllDepartments(){
+
+    public List<DepartmentResponseDto> getAllDepartments() {
         List<Department> departments = departmentRepository.findAll();
         List<DepartmentResponseDto> departmentResponseDtos = new ArrayList<>();
-        for ( Department department : departments){
-            departmentResponseDtos.add(new DepartmentResponseDto(department.getId(),department.getName(),department.getDescription()));
+        for (Department department : departments) {
+            departmentResponseDtos.add(new DepartmentResponseDto(department.getId(), department.getName(), department.getDescription()));
         }
 
         return departmentResponseDtos;
 
     }
+
+
+    public DepartmentResponseDto getDepartmentById(Long id) {
+        Department department = departmentRepository.findById(id).orElseThrow();
+        return new DepartmentResponseDto(
+                department.getId(),
+                department.getName(),
+                department.getDescription()
+        );
+
+
+    }
+    public DepartmentResponseDto updateDepartment(Long id, DepartmentRequestDto requestDto){
+        Department department = departmentRepository.findById(id).orElseThrow();
+        department.setDescription(requestDto.getDescription());
+        department.setName(requestDto.getName());
+        Department updatedDepartment = departmentRepository.save(department);
+        return new DepartmentResponseDto(updatedDepartment.getId(),updatedDepartment.getName(),updatedDepartment.getDescription());
+    }
+
+
 }
 
 
